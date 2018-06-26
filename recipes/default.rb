ff87855d0706d854ec1cc536e6ec1a0823921b71
capgemini complete001 <capgeminicomplete001@gmail.com>
2018-06-26 18:12:33 +0000

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [
+  {
+    "hostname" => "ec2-18-209-190-187.compute-1.amazonaws.com",
+    "ipaddress" => "18.209.190.187",
+    "port" => 80,
+    "ssl_port" => 80
+  },
+  {
+    "hostname" => "ec2-52-91-98-192.compute-1.amazonaws.com",
+    "ipaddress" => "52.91.98.192",
+    "port" => 80,
+    "ssl_port" => 80
+  }
+]
+
+include_recipe 'haproxy::manual'
+
